9d83796db0b7197f761f5e64cbb190e51fd1fe7d
André Grillo <andregrillo@gmail.com>
2025-10-08 13:12:40 +0100

    Standalone WKWebView without any UI components

--- src/ios/OSIABHiddenBrowserManager.swift
+++ src/ios/OSIABHiddenBrowserManager.swift
@@ -2,56 +2,106 @@ import Foundation
 import WebKit
 
 /// Manager for hidden InAppBrowser instances used for background authentication flows
-class OSIABHiddenBrowserManager {
+class OSIABHiddenBrowserManager: NSObject {
     static let shared = OSIABHiddenBrowserManager()
 
     private var hiddenBrowsers: [String: HiddenBrowserInstance] = [:]
 
-    private init() {}
+    private override init() {
+        super.init()
+    }
 
-    struct HiddenBrowserInstance {
+    class HiddenBrowserInstance: NSObject, WKNavigationDelegate {
         let webView: WKWebView
-        let viewModel: OSIABWebViewModel
+        let browserId: String
         var timer: Timer?
+        let completionHandler: (OSIABEventType, Any?) -> Void
+        private var firstLoadDone = false
+
+        init(browserId: String, url: URL, options: OSIABWebViewOptions, customHeaders: [String: String]?, timeout: Int?, completionHandler: @escaping (OSIABEventType, Any?) -> Void) {
+            self.browserId = browserId
+            self.completionHandler = completionHandler
+
+            // Create WKWebView configuration
+            let configuration = WKWebViewConfiguration()
+            configuration.allowsInlineMediaPlayback = options.allowInLineMediaPlayback
+            configuration.mediaTypesRequiringUserActionForPlayback = options.mediaPlaybackRequiresUserAction ? .all : []
+            configuration.suppressesIncrementalRendering = options.surpressIncrementalRendering
+
+            // Create a 1x1 hidden webview
+            self.webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 1, height: 1), configuration: configuration)
+
+            super.init()
+
+            // Configure WebView
+            self.webView.navigationDelegate = self
+            if let userAgent = options.customUserAgent {
+                self.webView.customUserAgent = userAgent
+            }
+
+            // Clear cache if needed
+            if options.clearCache {
+                let dataStore = WKWebsiteDataStore.default()
+                dataStore.removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), modifiedSince: Date(timeIntervalSince1970: 0)) { }
+            } else if options.clearSessionCache {
+                let dataStore = WKWebsiteDataStore.default()
+                let types = Set([WKWebsiteDataTypeCookies, WKWebsiteDataTypeSessionStorage])
+                dataStore.removeData(ofTypes: types, modifiedSince: Date(timeIntervalSince1970: 0)) { }
+            }
+
+            // Load URL with custom headers if provided
+            var request = URLRequest(url: url)
+            if let headers = customHeaders {
+                for (key, value) in headers {
+                    request.setValue(value, forHTTPHeaderField: key)
+                }
+            }
+            self.webView.load(request)
+
+            // Set up timeout if specified
+            if let timeout = timeout, timeout > 0 {
+                self.timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(timeout), repeats: false) { [weak self] _ in
+                    self?.completionHandler(.pageClosed, ["reason": "timeout"])
+                }
+            }
+        }
+
+        // WKNavigationDelegate methods
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            if !firstLoadDone {
+                firstLoadDone = true
+                completionHandler(.pageLoadCompleted, nil)
+            } else {
+                completionHandler(.pageNavigationCompleted, webView.url?.absoluteString)
+            }
+        }
+
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            completionHandler(.pageClosed, ["error": error.localizedDescription])
+        }
+
+        func cleanup() {
+            timer?.invalidate()
+            timer = nil
+            webView.stopLoading()
+            webView.navigationDelegate = nil
+        }
+
+        deinit {
+            cleanup()
+        }
     }
 
     /// Creates and stores a hidden browser instance
     func create(browserId: String, url: URL, options: OSIABWebViewOptions, customHeaders: [String: String]?, timeout: Int?, completionHandler: @escaping (OSIABEventType, Any?) -> Void) {
-        // Create a hidden WebView with 1x1 size (invisible but functional)
-        let config = OSIABWebViewConfigurationModel(
+        let instance = HiddenBrowserInstance(
+            browserId: browserId,
             url: url,
             options: options,
-            customHeaders: customHeaders
+            customHeaders: customHeaders,
+            timeout: timeout,
+            completionHandler: completionHandler
         )
-
-        let viewModel = OSIABWebViewModel(
-            config: config,
-            onDelegateClose: { [weak self] in
-                self?.remove(browserId: browserId)
-                completionHandler(.pageClosed, nil)
-            },
-            onDelegateURL: { _ in },
-            onDelegateAlertController: { _ in },
-            completionHandler: { event, data in
-                completionHandler(event, data)
-            }
-        )
-
-        // Create invisible webView
-        let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 1, height: 1), configuration: viewModel.webViewConfiguration)
-        viewModel.setupWebView(webView)
-        viewModel.loadURL()
-
-        var instance = HiddenBrowserInstance(webView: webView, viewModel: viewModel, timer: nil)
-
-        // Set up timeout if specified
-        if let timeout = timeout, timeout > 0 {
-            let timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(timeout), repeats: false) { [weak self] _ in
-                self?.remove(browserId: browserId)
-                completionHandler(.pageClosed, ["reason": "timeout"])
-            }
-            instance.timer = timer
-        }
 
         hiddenBrowsers[browserId] = instance
     }
@@ -59,9 +109,7 @@ class OSIABHiddenBrowserManager {
     /// Removes a hidden browser instance
     func remove(browserId: String) {
         guard let instance = hiddenBrowsers[browserId] else { return }
-
-        instance.timer?.invalidate()
-        instance.viewModel.cleanup()
+        instance.cleanup()
         hiddenBrowsers.removeValue(forKey: browserId)
     }
 
